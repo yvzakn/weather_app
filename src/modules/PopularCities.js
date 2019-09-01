79f4a3652e12673f36118f47d2609928168d7997
Yavuz Akin <yvz-akn@hotmail.com>
2019-08-31 19:12:27 -0500

    add new menu for cities

--- src/modules/PopularCities.js
+++ src/modules/PopularCities.js
@@ -1,45 +1,71 @@
-import { Button, Card, Row, Col } from 'antd';
+import { Button, Card, Row, Col, Menu, Icon } from 'antd';
 import React from 'react';
 import tr from '../assets/flags/tr.png';
 import eng from '../assets/flags/eng.png';
 import fr from '../assets/flags/fr.png';
+import snow from '../assets/weather/snow.png'
 
 class popularCities extends React.Component {
   state = {
     
   };
 
-
   getCityButtons = () => {
+    const { SubMenu } = Menu;
     return (
       <Row gutter={16}>
         <Col span={4} >
           <div style={{marginTop: '5px'}}>
-            <Button type="primary" style={{minWidth: '120px'}}>
-              <div>
-                <img src={tr} style={{height: "60%"}} alt={'no flag'} />
-                {' '}
-                {'Istanbul'}
-              </div>
-            </Button>
-            </div>
-            <div style={{marginTop: '5px'}}>
-            <Button type="primary" style={{minWidth: '120px'}}>
-              <div>
-                <img src={eng} style={{height: "60%"}} alt={'no flag'} />
-                {' '}
-                {'London'}
-              </div>
-            </Button>
-            </div>
-            <div style={{marginTop: '5px'}}>
-            <Button type="primary" style={{minWidth: '120px', textAlign: 'left'}}>
-              <div>
-                <img src={fr} style={{height: "60%"}} alt={'no flag'} />
-                {' '}
-                {'Paris'}
-              </div>
-            </Button>
+            <Menu
+              style={{ width: 256 }}
+              defaultOpenKeys={['sub1']}
+              selectedKeys={[this.state.current]}
+              mode="inline"
+            >
+              <SubMenu
+                key="sub1"
+                title={
+                  <div>
+                    <img src={tr} style={{height: "60%"}} alt={'no flag'} />
+                    {' '}
+                    {'Istanbul'}
+                  </div>
+                }
+              >
+                <Menu.Item key="1">Option 1</Menu.Item>
+                <Menu.Item key="2">Option 2</Menu.Item>
+                <Menu.Item key="3">Option 3</Menu.Item>
+                <Menu.Item key="4">Option 4</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub2"
+                title={
+                  <div>
+                    <img src={eng} style={{height: "60%"}} alt={'no flag'} />
+                    {' '}
+                    {'London'}
+                  </div>
+                }
+              >
+                <Menu.Item key="5">Option 5</Menu.Item>
+                <Menu.Item key="6">Option 6</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub4"
+                title={
+                  <div>
+                    <img src={fr} style={{height: "60%"}} alt={'no flag'} />
+                    {' '}
+                    {'Paris'}
+                  </div>
+                }
+              >
+                <Menu.Item key="9">Option 9</Menu.Item>
+                <Menu.Item key="10">Option 10</Menu.Item>
+                <Menu.Item key="11">Option 11</Menu.Item>
+                <Menu.Item key="12">Option 12</Menu.Item>
+              </SubMenu>
+            </Menu>
           </div>
         </Col>
         <Col span={20}>
@@ -49,7 +75,6 @@ class popularCities extends React.Component {
     );
   }
 
-
   getCard = () => {
 
     return (
@@ -57,7 +82,7 @@ class popularCities extends React.Component {
         hoverable
         cover={
           <div style={{ display: 'flex', justifyContent: 'center' }} >
-            <img style={{ width:"100px", margin: "10px" }} alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+            <img style={{ width:"100px", margin: "10px" }} alt="example" src={snow} />
           </div>
         }
       >
